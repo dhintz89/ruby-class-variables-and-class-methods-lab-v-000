497b1b80126db81c7dc761e93420c24a1a4888b3
Dan Hintz <dhintz@live.com>
2018-09-02 18:13:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,20 @@
 class Song
-  
+  @@count_of_songs = 0
+  @@artists = []
+  @@genres = {}
   
   def self.count
-    
+    @@count_of_songs
   end
   
   def self.artists
+    @@artists
+  end
+  
+  def self.genres
     
+  end
+  
+  def self.genre_count
+    @@genres.get_keys.count
   end
