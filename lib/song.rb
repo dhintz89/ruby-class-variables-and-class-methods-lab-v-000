5c463223e33757b4e68c7a800dabb5bbbb739f79
Dan Hintz <dhintz@live.com>
2018-09-02 18:57:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,16 @@ class Song
   end
   
   def self.artist_count
-    
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash.key?(genre)
+        genre_hash[genre] += 1
+      else 
+        genre_hash[genre] = []
+        genre_hash[genre] = 1
+      end
+    end
+    genre_hash    
   end
 
 end
